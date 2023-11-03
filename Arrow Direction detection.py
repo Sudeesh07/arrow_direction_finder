7089a7ff0d6e189524df9d14f2de1aca13e8ce37
Sudeesh <135733667+Sudeesh07@users.noreply.github.com>
2023-11-04 02:17:01 +0530

    arrow detection file

---
+++ Arrow Direction detection.py
@@ -0,0 +1,61 @@
+import cv2
+import math
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    if not ret:
+        break
+    edges = cv2.Canny(gray, 150, 250) 
+    contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    arrow_contour = None  
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if 4000<area:
+            arrow_contour = cnt
+            # print(area)
+            break
+            
+    if arrow_contour is not None:
+        epsilon = 0.02 * cv2.arcLength(arrow_contour, True)
+        approx = cv2.approxPolyDP(arrow_contour, epsilon, True)
+        vertices = approx.reshape((-1, 2))
+
+        for vertex in vertices:
+            cv2.circle(frame, (vertex[0], vertex[1]), 5, (0, 255, 0), -1) 
+        
+        x_midpoint = int(sum(vertices[:, 0]) / len(vertices))
+        y_midpoint = int(sum(vertices[:, 1]) / len(vertices))
+        cv2.circle(frame, (x_midpoint, y_midpoint), 5, (255, 0, 0), -1)  # Draw midpoint in red
+
+        direction_vector = (vertices[0][0] - x_midpoint, vertices[0][1] - y_midpoint)
+        angle = math.degrees(math.atan2(direction_vector[1], direction_vector[0]))
+        direction_text = f"Direction: {angle:.2f} degrees"
+        cv2.putText(frame, direction_text, (x_midpoint - 100, y_midpoint - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+
+
+        # if(-12<angle<14 ):
+        #     print("RIGHT")
+        # elif(-180<angle<-135):
+        #     print("left")
+
+        if(angle >= -45 and angle < 45):
+            print("RIGHT")
+        elif(angle >=45 and angle < 135):
+            print("DOWN")
+        elif(angle >= -180 and angle <=-135): 
+            print("LEFT")
+        elif(angle >=135 and angle <=180):
+            print("LEFT")
+
+
+    cv2.imshow('Detected Vertices', frame)
+
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+        break
+
+#print(vertices)
+
+cap.release()
+cv2.destroyAllWindows()
